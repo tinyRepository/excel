d40551f4ab47cd5dd1d65a746f0342e781ff8c51
Andrey <tinymail8@gmail.com>
2022-12-01 01:52:16 +0300

    add html & css

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,14 @@ module.exports = {
           from: path.resolve(__dirname, "src/favicon.ico"),
           to: path.resolve(__dirname, "dist"),
         },
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: path.resolve(__dirname, "dist/assets"),
+        },
+        {
+          from: path.resolve(__dirname, "src/images"),
+          to: path.resolve(__dirname, "dist/images"),
+        },
       ],
     }),
     new CleanWebpackPlugin(),
